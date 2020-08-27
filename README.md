b5b4dae89b43e31295dca3e06e528e5facbaf6b8
CM2Walki <walentinlamonos@googlemail.com>
2020-08-27 09:47:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # steamsdk
+(WIP)
 Dockerfile for automated build of a SteamCMD + SteamSDK installation
+
+Scripts Dir:
+"${STEAMSDKDIR}/sdk/tools/ContentBuilder/scripts"
+
+Content Dir:
+"${STEAMSDKDIR}/sdk/tools/ContentBuilder/content"
+
+Output Dir:
+"${STEAMSDKDIR}/sdk/tools/ContentBuilder/output"
---
+++ etc/entry.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Are there scripts in the directory?
+# This is done on entry to provide bind mount support
+if [ ! "$( ls -A ${BUILDERSCRIPTDIR} )" ]; then
+	# There are no scripts, download the default ones
+	wget -qO- "${DLURL}/master/etc/app_build_default.vdf" -O "${BUILDERSCRIPTDIR}/app_build_default.vdf"
+	wget -qO- "${DLURL}/master/etc/depot_build_default.vdf" -O "${BUILDERSCRIPTDIR}/depot_build_default.vdf"
+	
+	# Replace variables in app_build.vdf
+	sed -i -e 's/{{STEAMAPPID}}/'"${STEAMAPPID}"'/g' \
+		-e 's/{{STEAMDEPOTID}}/'"${STEAMDEPOTID}"'/g' \
+		-e 's/{{STEAMAPPBUILDESC}}/'"${STEAMAPPBUILDESC}"'/g' \
+		-e 's/{{STEAMAPPBRANCH}}/'"${STEAMAPPBRANCH}"'/g' \
+		"${BUILDERSCRIPTDIR}/app_build_default.vdf"
+
+	# Replace variables in depot_build.vdf
+	sed -i -e 's/{{STEAMDEPOTID}}/'"${STEAMDEPOTID}"'/g' \
+		-e 's|{{BUILDERCONTENTDIR}}|'"${BUILDERCONTENTDIR}"'|g' \
+		-e 's|{{LOCALCONTENTPATH}}|'"${LOCALCONTENTPATH}"'|g' \
+		"${BUILDERSCRIPTDIR}/depot_build_default.vdf"
+fi
+
+#bash "${STEAMCMDDIR}/steamcmd.sh" +login "${STEAMUSER}" "${STEAMPASSWORD}" ${STEAMGUARDCODE}" \
+#					+run_app_build "${BUILDERSCRIPTDIR}/${VDFAPPBUILD}"
+#					+quit
